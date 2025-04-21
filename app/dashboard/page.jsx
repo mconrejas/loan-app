e4ad5f565fc0ce34ca1dd5adec35ee207c601610
janedoe <janedo102@gmail.com>
2025-04-21 14:26:13 +0800

    update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import styles from '../styles/home.module.css';
+import "./styles.css";
 
 const Dashboard = () => {
   const [currentPage, setCurrentPage] = useState('personalInfo');
