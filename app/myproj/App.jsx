656e4bbd2388320ebcd3f197019df4f148450d5e
janedoe <janedo102@gmail.com>
2025-04-24 10:24:01 +0800

    update

--- app/myproj/App.jsx
+++ app/myproj/App.jsx
@@ -1,75 +1,13 @@
-import React, { useState } from 'react';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  Stack,
-  Menu,
-  MenuItem
-} from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import React from 'react';
+import { MuiNavbar } from './MuiNavbar';
 
-export const MuiNavbar = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+function App() {
   return (
-    <AppBar position="static" color="transparent">
-      <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-          <CatchingPokemonIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          POKEMONAPP
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Button color="inherit">Features</Button>
-          <Button color="inherit">Pricing</Button>
-          <Button color="inherit">About</Button>
-          <Button
-            color="inherit"
-            id="resources-button"
-            aria-controls={open ? 'resources-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            endIcon={<KeyboardArrowDownIcon />}
-            onClick={handleClick}
-          >
-            Resources
-          </Button>
-          <Button color="inherit">Login</Button>
-        </Stack>
-        <Menu
-          id="resources-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right'
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right'
-          }}
-          MenuListProps={{
-            'aria-labelledby': 'resources-button'
-          }}
-        >
-          <MenuItem onClick={handleClose}>Blog</MenuItem>
-          <MenuItem onClick={handleClose}>Podcast</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <MuiNavbar />
+      {/* Your other components go here */}
+    </div>
   );
-};
+}
+
+export default App;
