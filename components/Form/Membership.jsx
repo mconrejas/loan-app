ea32d5ff80e5ef25458ff37cbf3c82825657999a
janedoe <janedo102@gmail.com>
2025-04-25 13:55:02 +0800

    Membership.jsx

--- components/Form/Membership.jsx
+++ components/Form/Membership.jsx
@@ -7,36 +7,19 @@ import {
   FormControl,
   InputLabel,
   Select,
-  Menu,
   MenuItem,
   Typography,
   Stack,
   Checkbox,
   FormControlLabel,
   FormGroup,
-  AppBar,
-  Toolbar,
-  IconButton,
   Container,
-  Box,
   Paper,
-  useScrollTrigger,
-  Slide
+
 } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import LogoutIcon from '@mui/icons-material/Logout';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-function HideOnScroll({ children }) {
-  const trigger = useScrollTrigger();
 
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
+
 
 
 export default function MembershipForm() {
@@ -80,40 +63,6 @@ export default function MembershipForm() {
 
   return (
     <>
-      
-      <HideOnScroll>
-        <AppBar position="fixed" color="primary">
-          <Toolbar>
-            <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-              <AccountCircleIcon />
-            </IconButton>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              My Application
-            </Typography>
-            <Stack direction="row" spacing={2}>
-              <Button
-                color="inherit"
-                startIcon={<DashboardIcon />}
-                endIcon={<ArrowDropDownIcon />}
-                onClick={handleClick}
-              >
-                Dashboard
-              </Button>
-              <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                <MenuItem onClick={handleClose} sx={{ fontSize: '0.86rem' }}>Membership Application</MenuItem>
-                <MenuItem onClick={handleClose} sx={{ fontSize: '0.86rem' }}>Loan Application</MenuItem>
-              </Menu>
-              <Button
-                color="inherit"
-                startIcon={<LogoutIcon />}
-                onClick={() => alert('Logging out')}
-              >
-                Logout
-              </Button>
-            </Stack>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
 
       <Container maxWidth="md" sx={{ mt: 10, mb: 5 }}>
         <Paper elevation={3} sx={{ p: 4 }}>
